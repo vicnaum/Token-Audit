248267f5b71acc3edcf146d067ad8d70824b7b16
Victor Naumik <vicnaum@gmail.com>
2021-04-10 08:08:00 +0200

    config example update

--- config.example.js
+++ config.example.js
@@ -1,7 +1,9 @@
 const infuraApiKey = 'ReplaceWithKey000000';
 const mnemonic = 'your mcemonic will go here in this format';
+const privateKey = 'yourPrivateKeyHereWithout0x'
 
 module.exports = {
   infuraApiKey,
-  mnemonic
+  mnemonic,
+  privateKey
 };
